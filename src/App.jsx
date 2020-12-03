5adccba806226e4ac22c24a305d03458d0b528c7
Juan Carlos <juanksoliveira@gmail.com>
2020-12-03 13:48:40 -0300

    feat: add support for firebase

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,30 @@ import React, { useState, useEffect } from "react";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+import firebase from "firebase/app";
+import "firebase/firestore";
+import firebaseConfig from "./config/firebase";
+
 import { Provider } from "react-redux";
 import store from "./redux/store";
+
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 import MainView from "./views/MainView";
 import PostView from "./views/PostView";
 import LoginView from "./views/LoginView";
 
 function App() {
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
+      setLoading(false);
+    }
+  }, []);
+
+  if (isLoading) return <CircularProgress />;
   return (
     <Provider store={store}>
       <Router>
