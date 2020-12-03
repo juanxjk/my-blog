097347851a096128c3ca659f87143cee75052942
Juan Carlos <juanksoliveira@gmail.com>
2020-12-03 12:54:32 -0300

    feat(views): add initial design for PostView

--- src/views/PostView.jsx
+++ src/views/PostView.jsx
@@ -1,7 +1,45 @@
 import React, { useState, useEffect } from "react";
+import { useParams, Redirect } from "react-router-dom";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import CircularProgress from "@material-ui/core/CircularProgress";
+
+import Navbar from "../components/Navbar";
 function PostView() {
+  const { id } = useParams();
+  const [isLoading, setLoading] = useState(true);
+  const [_post, setPost] = useState();
+  if (!isLoading && !_post) return <Redirect to="/" />;
+
   return (
-    <div>PostView</div>
+    <Grid container justify="center">
+      <Navbar />
+      {isLoading ? (
+        <CircularProgress />
+      ) : (
+        _post && (
+          <Grid item sm={12}>
+            <Typography variant="h1">{_post.title}</Typography>
+            <Typography>views: {_post.views} </Typography>
+            <Typography>Date: {_post.createdAt} </Typography>
+            <Typography variant="h4" paragraph>
+              {_post.content}
+            </Typography>
+
+              <Button
+                variant="contained"
+                color="secondary"
+                justify="center"
+                startIcon={<FavoriteIcon />}
+              >
+                Like ({_post.likes})
+              </Button>
+          </Grid>
+        )
+      )}
+    </Grid>
   );
 }
 
